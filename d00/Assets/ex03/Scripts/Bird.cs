8f99b5526d5b7841f57ba76eb9ebcdc386bb6c89
Julian WOLF <jwolf@c4r24s3.wethinkcode.co.za>
2019-10-07 18:37:11 +0200

    Jank as hell

---
+++ d00/Assets/ex03/Scripts/Bird.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+	[SerializeField]
+	private	GameObject		birdie;
+	[SerializeField]
+	private	Vector3			newpos;
+	private Vector3			rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = new Vector3(0, 0, -45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+			newpos = new Vector3(0, 20 * Time.deltaTime, 0);
+		}
+		else
+		{
+			newpos = new Vector3(0, -1 * Time.deltaTime, 0);
+		}
+		if (birdie.transform.position.y >= -3.5f)
+			birdie.transform.Translate(newpos);
+    }
+}
---
+++ d00/Assets/ex03/Scripts/Pipe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[]		SpawnedPipes;
+	private	float				timeToSpawn;
+    Vector3						dirthing;
+	// Start is called before the first frame update
+    void Start()
+    {
+		timeToSpawn = 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		foreach(GameObject obj in SpawnedPipes)
+		{
+			dirthing = new Vector3(-3 * Time.deltaTime, 0, 0);
+			obj.transform.Translate(dirthing);
+			if (obj.transform.position.x <= -10f)
+			{
+				Vector3 newpos = new Vector3(10, 0, 0);
+				obj.transform.position = newpos;
+			}
+		}
+    }
+}
