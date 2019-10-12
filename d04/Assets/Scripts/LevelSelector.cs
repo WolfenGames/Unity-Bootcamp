82b0159e8824620f4f4662088c2e9edb2dfad7b6
Julian WOLF <jwolf@c4r24s3.wethinkcode.co.za>
2019-10-12 15:02:40 +0200

    auto commit

---
+++ d04/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+	public KeyCode	keyToPress;
+	public bool		start;
+	public string	SceneToLoad;
+
+	int			x,y;
+
+	[SerializeField]
+	GameObject[]	LevelSelectorThing;
+	public	  Image	LevelSelectorImg;
+	private void Start() {
+		LevelSelectorThing = GameObject.FindGameObjectsWithTag("LevelThing");
+		foreach(GameObject go in LevelSelectorThing)
+		{
+			go.GetComponentInChildren<Image>().color = Color.gray;
+		}
+	}
+	
+    void Update()
+    {
+		x = PlayerPrefsManager.ppm.Get_i("X");
+		y = PlayerPrefsManager.ppm.Get_i("Y");
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+			x--;
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			x++;
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+			y--;
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+			y++;
+		if (y > 2) y = 0;
+		if (y < 0) y = 2;
+
+		if (x > 3) x = 0;
+		if (x < 0) x = 3;
+		if (start)
+		{
+			if (Input.GetKeyDown(keyToPress))
+			{
+				SceneManager.LoadScene(SceneToLoad);
+			}
+		}
+		else
+		{
+			GameObject go = LevelSelectorThing[4 * y + x];
+
+			foreach(GameObject go2 in LevelSelectorThing)
+			{
+				go2.GetComponentInChildren<Image>().color = Color.gray;
+			}
+			if (go)
+			{
+				go.GetComponentInChildren<Image>().color = Color.white;
+				PlayerPrefsManager.ppm.Add("SelectedLevel", go?.transform?.GetChild(0)?.GetComponentInChildren<Text>()?.text);
+			}
+		}
+		PlayerPrefsManager.ppm.Add("X", x);
+		PlayerPrefsManager.ppm.Add("Y", y);
+    }
+}
---
+++ d04/Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+	public Text[]						infoGraphics;
+
+	public static PlayerPrefsManager 	ppm;
+
+	private void Start() {
+		ppm = this;
+		infoGraphics[0].text = Get_i("Lives").ToString();
+		infoGraphics[1].text = Get_i("Gold").ToString();
+		infoGraphics[3].text = string.Format("BEST SCORE {0} PTS", Get_i("MaxScore").ToString());
+	}
+
+	private void Update() {
+		infoGraphics[2].text = string.Format("{0}", Get_s("SelectedLevel").Length > 0 ? Get_s("SelectedLevel") : "No Level Selected");
+	}
+
+	public void Add(string name, int val) { PlayerPrefs.SetInt(name, val); }
+
+	public void Add(string name, string val) { PlayerPrefs.SetString(name, val); }
+	public void Add(string name, float val) { PlayerPrefs.SetFloat(name, val); }
+	public void Add(string name, bool val) { PlayerPrefs.SetString(name, val.ToString()); }
+
+
+	public float 	Get_f(string name){ return PlayerPrefs.GetFloat(name); }
+	public int	 	Get_i(string name){ return PlayerPrefs.GetInt(name); }
+	public bool 	Get_b(string name){ return bool.Parse(PlayerPrefs.GetString(name)); }
+	public string 	Get_s(string name){ return PlayerPrefs.GetString(name); }
+}
